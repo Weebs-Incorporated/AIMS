992d48ca7481025a48d6e152f3521aa8ceb7a57f
NachoToast <32235595+NachoToast@users.noreply.github.com>
2022-12-04 11:37:24 +1300

    update app test

--- src/app.test.ts
+++ src/app.test.ts
@@ -2,7 +2,7 @@ import request from 'supertest';
 import app from './app';
 
 describe('app', () => {
-    it('listens on a configured port', async () => {
+    it('returns status code 200', async () => {
         const response = await request(app).get('/').send();
 
         expect(response.statusCode).toBe(200);
