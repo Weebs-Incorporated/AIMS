16ee600a03a9cc454b2f8342ce0a118d8caf01a0
NachoToast <32235595+NachoToast@users.noreply.github.com>
2022-12-08 22:14:36 +1300

    update `validatorErrorHandler`

--- src/middleware/validatorErrorHandler.ts
+++ src/middleware/validatorErrorHandler.ts
@@ -2,15 +2,13 @@ import { ErrorRequestHandler } from 'express';
 import { HttpError } from 'express-openapi-validator/dist/framework/types';
 
 /** Custom error messages for OpenAPI validation errors. */
-export function validatorErrorHandler() {
-    const errorHandler: ErrorRequestHandler = (err, _req, res, next) => {
+export function validatorErrorHandler(): ErrorRequestHandler {
+    return (err, _req, res, next) => {
         if (err instanceof HttpError) {
-            res.status(500).json({
+            res.status(err.status).json({
                 message: err.message,
                 errors: err.errors,
             });
         } else next(err);
     };
-
-    return errorHandler;
 }
