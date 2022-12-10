d93853afe0c7f3d194ebed6454fbcf039d6f740c
NachoToast <32235595+NachoToast@users.noreply.github.com>
2022-12-10 18:40:10 +1300

    update app initialization

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,19 @@
+import { MongoClient } from 'mongodb';
 import { createApp } from './app';
 import { getConfig } from './config';
 
 const config = getConfig();
 
-const app = createApp(config);
+const mongoClient = new MongoClient(config.mongoURI);
+
+const db = mongoClient.db(config.mongoDbName);
+
+const app = createApp(config, db);
 
 app.listen(config.port, () => {
     console.log(`Listening on port ${config.port}`);
+});
+
+mongoClient.connect().then(() => {
+    console.log('Connected to MongoDB');
 });
