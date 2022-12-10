19c68f1fb6508b8e838f63009cc4dfc4555ba7a7
NachoToast <32235595+NachoToast@users.noreply.github.com>
2022-12-10 18:25:32 +1300

    add newline spacing in `LoginResponse`

--- src/types/Responses/LoginResponse.ts
+++ src/types/Responses/LoginResponse.ts
@@ -2,12 +2,16 @@ import { User } from '../User';
 
 export interface LoginResponse {
     userData: User;
+
     /** Token to use for extending this user's current session. */
     refreshToken: string;
-    /** How long until the current siteToken expires, in seconds. */
+
+    /** How long until the current {@link siteToken} expires, in seconds. */
     expiresInSeconds: number;
+
     /** Signed JWT to send in `Authorization` header for any elevated requests to this API. */
     siteToken: string;
+
     /** Typeof operation this was generated from. */
     type: 'login' | 'refresh' | 'register';
 }
