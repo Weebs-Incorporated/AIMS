3479186b483d8dac9a35ae6056888cc0fe7c68d6
NachoToast <32235595+NachoToast@users.noreply.github.com>
2022-12-11 15:07:22 +1300

    Revert "add defaults to `EndpointProvider` generics"
    This reverts commit a9be106b7a4d5bc9552f619104d0d3cda56f419b.

--- src/types/Express/EndpointProvider.ts
+++ src/types/Express/EndpointProvider.ts
@@ -3,7 +3,7 @@ import { Db } from 'mongodb';
 import { Config } from '../../config';
 import { ResponseLocals } from './ResponseLocals';
 
-export type EndpointProvider<TRequest = never, TResponse = never> = (
+export type EndpointProvider<TRequest, TResponse> = (
     config: Config,
     db?: Db,
 ) => RequestHandler<object, TResponse, TRequest, object, ResponseLocals>;
