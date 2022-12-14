cd2a8cecf4ab869f6322c717954c98d2917bbef3
NachoToast <32235595+NachoToast@users.noreply.github.com>
2022-12-14 18:56:20 +1300

    add query parameter typing to endpoint provider

--- src/types/Express/EndpointProvider.ts
+++ src/types/Express/EndpointProvider.ts
@@ -10,20 +10,18 @@ export interface AppDatabaseCollections {
 }
 
 /** Endpoint handler with typed request body and response types. */
-export type EndpointProviderReturnValue<TRequest = unknown, TResponse = unknown> = RequestHandler<
-    object,
-    TResponse,
-    TRequest,
-    object,
-    ResponseLocals
->;
+export type EndpointProviderReturnValue<
+    TRequest = unknown,
+    TResponse = unknown,
+    TQueryParams = object,
+> = RequestHandler<TQueryParams, TResponse, TRequest, object, ResponseLocals>;
 
-interface EndpointProviderNoScopes<TRequest, TResponse> {
+interface EndpointProviderNoScopes<TRequest, TResponse, TQueryParams> {
     scopes: 'none';
-    applyToRoute: ({ config }: { config: Config }) => EndpointProviderReturnValue<TRequest, TResponse>;
+    applyToRoute: ({ config }: { config: Config }) => EndpointProviderReturnValue<TRequest, TResponse, TQueryParams>;
 }
 
-interface EndpointProviderDbScope<TRequest, TResponse> {
+interface EndpointProviderDbScope<TRequest, TResponse, TQueryParams> {
     scopes: 'db';
     applyToRoute: ({
         config,
@@ -31,10 +29,10 @@ interface EndpointProviderDbScope<TRequest, TResponse> {
     }: {
         config: Config;
         db: AppDatabaseCollections;
-    }) => EndpointProviderReturnValue<TRequest, TResponse>;
+    }) => EndpointProviderReturnValue<TRequest, TResponse, TQueryParams>;
 }
 
-interface EndpointProviderAuthScope<TRequest, TResponse> {
+interface EndpointProviderAuthScope<TRequest, TResponse, TQueryParams> {
     scopes: 'auth';
     applyToRoute: ({
         config,
@@ -42,10 +40,10 @@ interface EndpointProviderAuthScope<TRequest, TResponse> {
     }: {
         config: Config;
         auth: SiteTokenPayload;
-    }) => EndpointProviderReturnValue<TRequest, TResponse>;
+    }) => EndpointProviderReturnValue<TRequest, TResponse, TQueryParams>;
 }
 
-export interface EndpointProviderBothScopes<TRequest, TResponse> {
+export interface EndpointProviderBothScopes<TRequest, TResponse, TQueryParams> {
     scopes: 'both';
     applyToRoute: ({
         config,
@@ -55,12 +53,12 @@ export interface EndpointProviderBothScopes<TRequest, TResponse> {
         config: Config;
         db: AppDatabaseCollections;
         auth: SiteTokenPayload;
-    }) => EndpointProviderReturnValue<TRequest, TResponse>;
+    }) => EndpointProviderReturnValue<TRequest, TResponse, TQueryParams>;
 }
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export type EndpointProvider<TRequest = any, TResponse = any> =
-    | EndpointProviderNoScopes<TRequest, TResponse>
-    | EndpointProviderDbScope<TRequest, TResponse>
-    | EndpointProviderAuthScope<TRequest, TResponse>
-    | EndpointProviderBothScopes<TRequest, TResponse>;
+export type EndpointProvider<TRequest = any, TResponse = any, TQueryParams = object> =
+    | EndpointProviderNoScopes<TRequest, TResponse, TQueryParams>
+    | EndpointProviderDbScope<TRequest, TResponse, TQueryParams>
+    | EndpointProviderAuthScope<TRequest, TResponse, TQueryParams>
+    | EndpointProviderBothScopes<TRequest, TResponse, TQueryParams>;
