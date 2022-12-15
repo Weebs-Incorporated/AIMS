d95c08e22467cafdd9fad35b5855bdc268dc28ee
NachoToast <32235595+NachoToast@users.noreply.github.com>
2022-12-15 20:25:42 +1300

    add `Pagination` types

---
+++ src/types/Pagination/Pagination.ts
@@ -0,0 +1,10 @@
+export interface PaginationInput {
+    /** Starts at 0. */
+    page: number;
+    /** 1 - 100 (inclusive). */
+    perPage: number;
+}
+
+export interface PaginationResponse {
+    itemCount: number;
+}
