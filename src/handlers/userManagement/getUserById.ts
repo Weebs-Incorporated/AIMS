40c409fb67379a1bbe319b9d42df6a5a7abbd56d
NachoToast <32235595+NachoToast@users.noreply.github.com>
2022-12-15 20:25:35 +1300

    tweak `getUserById` endpoint

--- src/handlers/userManagement/getUserById.ts
+++ src/handlers/userManagement/getUserById.ts
@@ -7,22 +7,21 @@ export const getUserById: EndpointProvider<void, ClientFacingUser, { id: string 
         return async (req, res) => {
             const { id } = req.params;
 
-            const user = await db.users.findOne({ _id: id });
+            const user = await db.users.findOne<ClientFacingUser>({ _id: id });
 
             if (user === null) return res.sendStatus(404);
 
-            const safeUser: ClientFacingUser = user;
-
-            if (auth === null) safeUser.latestIp = null; // requester not logged in
-            else if (auth.id !== safeUser._id) {
+            if (auth === null) user.latestIp = null; // requester not logged in
+            else if (auth.id !== user._id) {
                 // requester logged in and not requesting self
-                const user = await db.users.findOne({ _id: auth.id });
+
+                const requester = await db.users.findOne({ _id: auth.id });
 
                 // requester account deleted, or doesn't have owner permissions
-                if (user === null || !hasPermission(user, UserPermissions.Owner)) safeUser.latestIp = null;
+                if (requester === null || !hasPermission(requester, UserPermissions.Owner)) user.latestIp = null;
             }
 
-            return res.status(200).json(safeUser);
+            return res.status(200).json(user);
         };
     },
 };
